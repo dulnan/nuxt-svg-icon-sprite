b4bb03c8d0d0ac28af38fa1426fb00ed64f493fb
Karl Swedberg <kswedberg@gmail.com>
2025-02-04 13:56:06 -0500

    Add spritePaths property to return of useSpriteData

--- src/runtime/composables/useSpriteData.ts
+++ src/runtime/composables/useSpriteData.ts
@@ -1,10 +1,12 @@
 import type { NuxtSvgSpriteSymbol } from '#nuxt-svg-sprite/runtime'
+import { SPRITE_PATHS } from '#nuxt-svg-sprite/runtime'
 import { ALL_SYMBOL_KEYS } from '#nuxt-svg-sprite/data'
 
 type SpriteData = {
   symbols: NuxtSvgSpriteSymbol[]
+  spritePaths: typeof SPRITE_PATHS
 }
 
 export default function (): SpriteData {
-  return { symbols: ALL_SYMBOL_KEYS }
+  return { symbols: ALL_SYMBOL_KEYS, spritePaths: SPRITE_PATHS }
 }
